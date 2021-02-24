c0b4b6be2c3011baf3dd1615c91040af44861b55
Jisie DM <jisie.david@gmail.com>
2021-02-24 12:13:09 -0800

    Kennel component uses data from js object

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -1,13 +1,28 @@
 import React from "react"
 import "./Kennel.css"
 
-export const Kennel = () => (
-  <>
-    <h2>Nashville Kennels</h2>
-    <small>Loving care when you're not there.</small>
-    <address>
-      <div>Visit Us at the Nashville North Location</div>
-      <div>500 Puppy Way</div>
-    </address>
-  </>
-)
+export const Kennel = () => {
+  const kennel = {
+    name: "Nashville Kennels: #1 in Davidson County",
+    locations: [
+      {
+        name: "Nashville North",
+        address: "500 Puppy Way"
+      }
+    ]
+  }
+
+  // debugger
+
+  return (
+    <>
+      {console.log("kennel object", kennel.locations)}
+      <h2>{kennel.name}</h2>
+      <small>Loving care when you're not there.</small>
+      <address>
+        <div>Visit Us at the {kennel.locations[0].name} Location</div>
+        <div>{kennel.locations[0].address}</div>
+      </address>
+    </>
+  )
+}
