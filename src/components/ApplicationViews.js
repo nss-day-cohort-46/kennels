d507214458c41651d8c35d69890233d56ec25d5d
Jisie DM <jisie.david@gmail.com>
2021-03-01 08:11:30 -0800

    Routes for all links in place

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -2,6 +2,10 @@ import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
 import { AnimalList } from "./animal/AnimalList"
+import { AnimalProvider } from "./animal/AnimalProvider"
+import { LocationList } from "./location/LocationList"
+import { CustomerList } from "./customer/CustomerList"
+import { EmployeeList } from "./employee/EmployeeList"
 
 export const ApplicationViews = () => {
   return (
@@ -13,7 +17,21 @@ export const ApplicationViews = () => {
 
       {/* Render the animal list when http://localhost:3000/animals */}
       <Route path="/animals">
-        <AnimalList />
+        <AnimalProvider>
+          <AnimalList />
+        </AnimalProvider>
+      </Route>
+
+      <Route path="/locations">
+        <LocationList />
+      </Route>
+
+      <Route path="/customers">
+        <CustomerList />
+      </Route>
+
+      <Route path="/employees">
+        <EmployeeList />
       </Route>
     </>
   )
