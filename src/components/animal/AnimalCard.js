31ed84d4b0e32cbadede393bc2dc0b4dbc1f0387
Jisie DM <jisie.david@gmail.com>
2021-03-01 10:28:42 -0800

    Placeholder for location and customer on AnimalCard

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -6,5 +6,7 @@ export const AnimalCard = ({animalProps}) => (
     {/* {console.log(animalProps)} */}
     <h3 className="animal__name">{animalProps.name}</h3>
     <div className="animal__breed">Breed: {animalProps.breed}</div>
+    <div className="animal__location">Location: Name of location</div>
+    <div className="animal__customer">Customer: Name of customer</div>
   </section>
 )
